15f361911516cacffbb533698eea991ae9b68a3f
Gilbert Ssenyonjo <gssenyonjo@gmail.com>
2024-04-10 16:57:45 +0300

    looked at resuming a suspend continuation

---
+++ src/main/kotlin/Resume.kt
@@ -0,0 +1,45 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+suspend fun main1() {
+
+    val i: Int = suspendCoroutine<Int> { cont ->
+        cont.resume(42)
+    }
+    println(i)
+
+    val str: String = suspendCoroutine {
+        it.resume("Hello")
+    }
+    println(str)
+
+    val b: Boolean = suspendCoroutine {
+        it.resume(true)
+    }
+    println(b)
+}
+
+suspend fun main() {
+    println("Start")
+
+    val user: User = requestUser()
+    println(user)
+
+    println("End")
+}
+
+suspend fun requestUser() : User {
+    delay(1000)
+    return suspendCancellableCoroutine <User> {
+        it.resume(User("Jane", "Eyre"))
+    }
+}
+
+
+data class User(
+    val firstName: String,
+    val lastName: String
+)
